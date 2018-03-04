7762e79bde7cdf2ff2a40aaa1f82290580eb87d8
RahulChidurala <rahul.chidurala@okstate.edu>
2018-03-03 23:30:29 -0600

    Hook up indoor breathing button.

--- app/src/main/java/com/hacklahoma/stressrelief/stressrelief/UseCases/IndoorActivities/IndoorStretching/IndoorStrecthingActivity.kt
+++ app/src/main/java/com/hacklahoma/stressrelief/stressrelief/UseCases/IndoorActivities/IndoorStretching/IndoorStrecthingActivity.kt
@@ -2,7 +2,13 @@ package com.hacklahoma.stressrelief.stressrelief.UseCases.IndoorActivities.Indoo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.ImageButton
 import com.hacklahoma.stressrelief.stressrelief.R
+import com.hacklahoma.stressrelief.stressrelief.UseCases.IndoorActivityMenu.IIndoorController
+import com.hacklahoma.stressrelief.stressrelief.UseCases.IndoorActivityMenu.IIndoorRouter
+import com.hacklahoma.stressrelief.stressrelief.UseCases.IndoorActivityMenu.IndoorController
+import com.hacklahoma.stressrelief.stressrelief.UseCases.IndoorActivityMenu.IndoorRouter
 
 /**
  * Created by chrah on 3/3/2018.
@@ -13,5 +19,6 @@ class IndoorStrecthingActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_indoor_stretching)
+
     }
 }
--- app/src/main/java/com/hacklahoma/stressrelief/stressrelief/UseCases/IndoorActivityMenu/IndoorActivity.kt
+++ app/src/main/java/com/hacklahoma/stressrelief/stressrelief/UseCases/IndoorActivityMenu/IndoorActivity.kt
@@ -2,6 +2,8 @@ package com.hacklahoma.stressrelief.stressrelief.UseCases.IndoorActivityMenu
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.ImageButton
 import com.hacklahoma.stressrelief.stressrelief.R
 
 /**
@@ -9,9 +11,22 @@ import com.hacklahoma.stressrelief.stressrelief.R
  */
 class IndoorActivity: AppCompatActivity() {
 
+    private lateinit var router: IIndoorRouter
+    private lateinit var controller: IIndoorController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_indoors)
+
+        router = IndoorRouter(this)
+        controller = IndoorController()
+        controller.router = router
+
+        val img_btn_breathe = findViewById<ImageButton>(R.id.img_btn_breathe)
+        img_btn_breathe.setOnClickListener(View.OnClickListener {
+
+            controller.actionToBreathing()
+        })
     }
 }
