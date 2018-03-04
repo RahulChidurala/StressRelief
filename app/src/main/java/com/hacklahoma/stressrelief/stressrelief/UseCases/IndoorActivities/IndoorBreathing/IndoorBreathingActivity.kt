f43341af0670df049a06c8e58b540ca0c52cc079
RahulChidurala <rahul.chidurala@okstate.edu>
2018-03-03 20:31:00 -0600

    Add blank breathing and stretching indoor activities, controllers, and routers.

---
+++ app/src/main/java/com/hacklahoma/stressrelief/stressrelief/UseCases/IndoorActivities/IndoorBreathing/IndoorBreathingActivity.kt
@@ -0,0 +1,17 @@
+package com.hacklahoma.stressrelief.stressrelief.UseCases.IndoorActivities.IndoorBreathing
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.hacklahoma.stressrelief.stressrelief.R
+
+/**
+ * Created by chrah on 3/3/2018.
+ */
+class IndoorBreathingActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_indoor_breathing)
+    }
+}
